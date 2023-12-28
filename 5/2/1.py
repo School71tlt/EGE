78c16c69edcaf350ff6162ead935804482a9408b
School71tlt <148452454+School71tlt@users.noreply.github.com>
2023-12-28 08:50:30 +0400

    Delete 5/2 directory

--- 5/2/1.py
+++
@@ -1,7 +0,0 @@
-print('w x y z F')
-for w in range(2):
-    for x in range(2):
-        for y in range(2):
-            for z in range(2):
-                if not ((x and not y) or (y == z) or not w):
-                    print(w, x, y, z, 0)
--- 5/2/5.py
+++
@@ -1,5 +0,0 @@
-print('a b c F')
-for a in range(2):
-    for b in range(2):
-        for c in range(2):
-            print(a, b, c, ((a and c) or (not a and (b or not c))))
